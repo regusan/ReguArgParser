34d8dab1d93d15f6eb7378b25ed8ce533777a56a
regusan-laptop-ubuntu <dammy@gmail.com>
2025-07-27 18:09:50 +0900

    [追加]単体テストの拡充

--- test/test.cpp
+++ test/test.cpp
@@ -29,17 +29,17 @@ IUTEST(ArgParserTest, HasFlagWithMixedFlags)
 
 IUTEST(ArgParserTest, CheckValue)
 {
-    ArgParser args({"-a", "value1", "-b", "value2"});
+    ArgParser args({"-a", "value1", "-b", "value2", "-c", "3", "-d", "3.14", "-e", "hoge,fuga,piyo"});
     ASSERT_EQ(args.getFlagValue<std::string>({{"-a"}}), "value1");
     ASSERT_EQ(args.getFlagValue<std::string>({{"-b"}}), "value2");
-
-    // デフォルト値のテスト
-    ASSERT_EQ(args.getFlagValue<std::string>({{"-c"}}, std::string("default")), "default");
+    ASSERT_EQ(args.getFlagValue<int>({{"-c"}}), 3);
+    ASSERT_EQ(args.getFlagValue<double>({{"-d"}}), 3.14);
+    ASSERT_EQ(args.getFlagValue<std::string>({{"-e"}}), "hoge,fuga,piyo");
 }
 
 IUTEST(ArgParserTest, GetFlagArrayValue)
 {
-    ArgParser args({"-a", "1,2,3", "-b", "4,5,6"});
+    ArgParser args({"-a", "1,2,3", "-b", "4,5,6", "-c", "7.1,7.2,7.3", "-d", "hoge,fuga,piyo"});
     auto arrayValueA = args.getFlagArrayValue<int>({{"-a"}});
     auto arrayValueB = args.getFlagArrayValue<int>({{"-b"}});
 
@@ -52,6 +52,24 @@ IUTEST(ArgParserTest, GetFlagArrayValue)
     ASSERT_EQ(arrayValueB[0], 4);
     ASSERT_EQ(arrayValueB[1], 5);
     ASSERT_EQ(arrayValueB[2], 6);
+    auto arrayValueC = args.getFlagArrayValue<double>({{"-c"}});
+    ASSERT_EQ(arrayValueC.size(), 3);
+    ASSERT_EQ(arrayValueC[0], 7.1);
+    ASSERT_EQ(arrayValueC[1], 7.2);
+    ASSERT_EQ(arrayValueC[2], 7.3);
+
+    auto arrayValueD = args.getFlagArrayValue<std::string>({{"-d"}});
+    ASSERT_EQ(arrayValueD.size(), 3);
+    ASSERT_EQ(arrayValueD[0], "hoge");
+    ASSERT_EQ(arrayValueD[1], "fuga");
+    ASSERT_EQ(arrayValueD[2], "piyo");
+
+    // デフォルト値のテスト
+    auto arrayValueE = args.getFlagArrayValue<int>({{"-e"}}, {10, 20, 30});
+    ASSERT_EQ(arrayValueE.size(), 3);
+    ASSERT_EQ(arrayValueE[0], 10);
+    ASSERT_EQ(arrayValueE[1], 20);
+    ASSERT_EQ(arrayValueE[2], 30);
 }
 
 IUTEST(ArgParserTest, GetFlagValueWithDefault)
@@ -59,6 +77,14 @@ IUTEST(ArgParserTest, GetFlagValueWithDefault)
     ArgParser args({"-a", "value1"});
     ASSERT_EQ(args.getFlagValue<std::string>({{"-a"}}), "value1");
     ASSERT_EQ(args.getFlagValue<std::string>({{"-b"}}, std::string("default")), "default");
+}
+
+// エラーのテスト
+IUTEST(ArgParserTest, GetFlagValueError)
+{
+    ArgParser args({"-a", "value1"});
+    ASSERT_THROW(args.getFlagValue<int>({{"-b"}}), std::runtime_error);
+    ASSERT_THROW(args.getFlagValue<double>({{"-c"}}), std::runtime_error);
 }
 
 int main(int argc, char const *argv[])
